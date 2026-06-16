7672277c9ed41fa1185d0863a307fe4f032498cc
Zapfire1 <sanlan14@vgs.nfk.no>
2026-06-16 14:05:50 +0200

    endret litt på strukturen, dobbel-sjekket at det var riktig SDK. alt er rett, men nett-applikasjon fungerer fortsatt ikke, dersom den ikke vil kjøres.

--- js/firebase.js
+++ js/firebase.js
@@ -1,14 +1,10 @@
-// Import the functions you need from the SDKs you need
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
   import { getFirestore } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+  // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyB_M1rTcvwDgRwivlBN6jrk4jUvcEjcgAo",
   authDomain: "it-hendelser-helse-stasjon.firebaseapp.com",
@@ -18,6 +14,8 @@ const firebaseConfig = {
   appId: "1:10914276100:web:8c2a3342b06b11248a2651"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // eksporter databasen så andre filer kan bruke den
+  export const db = getFirestore(app);
