a8df8e75d1c0f01304335c39e78963afd3e83f1e
Zapfire1 <sanlan14@vgs.nfk.no>
2026-06-16 13:43:46 +0200

    satte opp god mappestruktur. la til SDK i firebase.js og koblet til CSS i index.

---
+++ js/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB_M1rTcvwDgRwivlBN6jrk4jUvcEjcgAo",
+  authDomain: "it-hendelser-helse-stasjon.firebaseapp.com",
+  projectId: "it-hendelser-helse-stasjon",
+  storageBucket: "it-hendelser-helse-stasjon.firebasestorage.app",
+  messagingSenderId: "10914276100",
+  appId: "1:10914276100:web:8c2a3342b06b11248a2651"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
